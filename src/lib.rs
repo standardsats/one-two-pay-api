b5057f10049747b075a3ee1bc90eaabf5f3af156
Anton Gushcha <ncrashed@protonmail.com>
2023-09-16 18:52:33 +0700

    Stubs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Client {
+
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Error {
+
+}
+
+pub struct TransferReq {
+
+}
+
+pub struct TransferRes {
+
+}
+
+pub struct QueryReq {
+
+}
+
+pub struct QueryRes {
+
+}
+
+impl Client {
+    pub async fn transfer(client: &Client, args: TransferReq) -> Result<TransferRes, Error> {
+        todo!();
+    }
+
+    pub async fn query(client: &Client, args: QueryReq) -> Result<QueryRes, Error> {
+        todo!();
+    }
+    
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +41,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        // let result = add(2, 2);
+        // assert_eq!(result, 4);
     }
 }
